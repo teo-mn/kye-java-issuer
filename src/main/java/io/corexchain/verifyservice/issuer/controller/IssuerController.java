db9a3d92dd1c257c8ca58cf34108765f4967b21c
Surenbayar <suugii.sd@gmail.com>
2022-09-30 14:21:10 +0800

    update

--- src/main/java/io/corexchain/verifyservice/issuer/controller/IssuerController.java
+++ src/main/java/io/corexchain/verifyservice/issuer/controller/IssuerController.java
@@ -7,12 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import io.corexchain.verifyservice.issuer.exceptions.*;
 
 import javax.validation.Valid;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 @RestController
@@ -21,21 +24,29 @@ public class IssuerController {
     private static final Logger logger = LoggerFactory.getLogger(IssuerController.class);
 
     private IssuerService service;
+
     public IssuerController(IssuerService service) {
         this.service = service;
     }
 
+
+    @Value("${verify.service.file.directory}")
+    private String rootPath;
+
     @PostMapping("issue")
     public ResponseEntity<IssueResponseDTO> issue(@Valid @RequestBody IssueRequestDTO body) throws Exception {
         if (Objects.isNull(body)) throw new BadRequestException("Дата хоосон байж болохгүй");
-        IssueResponseDTO res = this.service.issue(body.sourcePath, body.destinationPath, body.expireDate, body.desc);
+        Path rootPath = Paths.get(this.rootPath);
+        IssueResponseDTO res = this.service.issue(rootPath.resolve(body.sourcePath).toString(),
+                rootPath.resolve(body.destinationPath).toString(), body.expireDate, body.desc);
         return ResponseEntity.ok(res);
     }
 
     @PostMapping("issue-test")
     public ResponseEntity<IssueResponseDTO> issueTest(@Valid @RequestBody IssueRequestDTO body) throws Exception {
         if (Objects.isNull(body)) throw new BadRequestException("Дата хоосон байж болохгүй");
-        IssueResponseDTO res = this.service.issueTest(body.sourcePath, body.destinationPath, body.expireDate, body.desc);
+        IssueResponseDTO res = this.service.issueTest(this.rootPath + body.sourcePath,
+                this.rootPath + body.destinationPath, body.expireDate, body.desc);
         return ResponseEntity.ok(res);
     }
 
